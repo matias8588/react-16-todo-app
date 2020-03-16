41f748d0ceeae43015000120bed22b19ce1f303d
Matias Cejas <matiascejas8@gmail.com>
2020-03-16 15:18:38 -0300

    Added index for palette

--- src/ui/theme/index.js
+++ src/ui/theme/index.js
@@ -1,11 +1,10 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme } from "@material-ui/core";
 
 const theme = createMuiTheme({
-    palette: {
-        primary: { main: "#f44336" },
-        secondary: { main: '#607d8b' },
-
-    },
+  palette: {
+    primary: { main: "#f44336" },
+    secondary: { main: "#607d8b" }
+  }
 });
 
 export default theme;
