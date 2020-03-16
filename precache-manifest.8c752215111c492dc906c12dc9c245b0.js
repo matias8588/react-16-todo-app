99f84c5a4383a306580acc648231b1f8ec9ff091
Matias Cejas <matiascejas8@gmail.com>
2020-03-16 16:52:14 -0300

    Updates

---
+++ precache-manifest.8c752215111c492dc906c12dc9c245b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383aba45ef5df5ecaa5304a3c2a4a5f8",
+    "url": "/react-16-todo-app/index.html"
+  },
+  {
+    "revision": "bbf7330fb29286cf5077",
+    "url": "/react-16-todo-app/static/css/main.b7cb2b5c.chunk.css"
+  },
+  {
+    "revision": "fe79edb7826f5174a398",
+    "url": "/react-16-todo-app/static/js/2.e1f35437.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-16-todo-app/static/js/2.e1f35437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf7330fb29286cf5077",
+    "url": "/react-16-todo-app/static/js/main.e2eed4af.chunk.js"
+  },
+  {
+    "revision": "300ce410b81eed52f187",
+    "url": "/react-16-todo-app/static/js/runtime-main.093fc357.js"
+  }
+]);
