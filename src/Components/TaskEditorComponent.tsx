4ebf3ab32744cfd7a16081eaf598335eed726d61
Matias Cejas <matiascejas8@gmail.com>
2020-03-17 09:53:02 -0300

    Added modal

--- src/Components/TaskEditorComponent.tsx
+++ src/Components/TaskEditorComponent.tsx
@@ -1,41 +1,61 @@
-import React from "react";
-import { TableRow, TableCell, Checkbox, IconButton } from "@material-ui/core";
-import DeleteIcon from "@material-ui/icons/Delete";
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-export const TaskRowEditor = (props: any) => (
-  <>
-    <TableRow key={props.task.name}>
-      <TableCell component="th" scope="row">
-        {props.task.name}
-      </TableCell>
-      <TableCell align="right">
-        <Checkbox
-          defaultChecked
-          color="primary"
-          value="default"
-          inputProps={{ "aria-label": "checkbox with default color" }}
-          checked={props.task.done}
-          onChange={() => props.toggleTask(props.task)}
-        />
-      </TableCell>
-      <TableCell align="right">
-        <IconButton
-          aria-label="edit"
-          color="secondary"
-          onClick={() => props.deleteTask(props.task)}
-        >
-          <DeleteIcon />
-        </IconButton>
-      </TableCell>
-      <TableCell align="right">
-        <IconButton
-          aria-label="delete"
-          color="secondary"
-          onClick={() => props.deleteTask(props.task)}
-        >
-          <DeleteIcon />
-        </IconButton>
-      </TableCell>
-    </TableRow>
-  </>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }),
 );
+
+export default function TransitionsModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-transition-group
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
